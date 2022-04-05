d174fcd6b502d72ade49435eb04fdd107a908bcd
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-04-05 16:07:41 -0500

    Feat: controller endpoints

--- src/modules/employees/employees.controller.ts
+++ src/modules/employees/employees.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateFromNetsuiteDTO } from '~core/dto/create-from-netsuite.dto';
 import { EmployeesService } from './employees.service';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+
+  @Post(':id/netsuite')
+  async createFromNetsuite(@Body() dto: CreateFromNetsuiteDTO) {
+    return await this.employeesService.create(dto.fields);
+  }
 }
