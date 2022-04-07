b17646ad92f1072b1cd0e63e1acba4e8b4a813fc
Fernando Delgado <fernandod@tissini.com>
2022-04-06 22:13:55 -0500

    .build to .cloud on domain config

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -26,7 +26,7 @@ const COOKIES_OPTIONS: CookieOptions = {
   sameSite: 'none',
   maxAge: EXP_TIME,
   secure: true,
-  domain: 'tissini.cloud', // ! TODO pass to env
+  domain: 'tissini.cloud',
 };
 
 @Controller({
