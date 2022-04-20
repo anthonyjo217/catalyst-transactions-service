32de0b0c88edb0de35afef5687e405c54f041908
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-04-20 11:36:01 -0500

    Fix mongourl

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { EmailModule } from './modules/email/email.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    MongooseModule.forRoot('mongodb://localhost/nest-crm'),
+    MongooseModule.forRoot(process.env.MONGODB_HOST),
     EmployeesModule,
     CustomerLeadsModule,
     AuthModule,
