7892d7ff1a3d53e0ba2d7fe39d85697789e2f6b4
Fernando Delgado <fernandod@tissini.com>
2022-05-02 12:18:36 -0500

    build scripts update

--- build-push-img-dev.sh
+++ build-push-img-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -f ./Dockerfile . -t ferchoslim/dev-catalyst-user-service:latest
+docker buildx build --platform linux/amd64 -f ./Dockerfile . -t ferchoslim/dev-catalyst-user-service:latest
 docker push ferchoslim/dev-catalyst-user-service:latest
--- build-push-img-prod.sh
+++ build-push-img-prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -f ./Dockerfile . -t ferchoslim/catalyst-user-service:latest
+docker buildx build --platform linux/amd64 -f ./Dockerfile . -t ferchoslim/catalyst-user-service:latest
 docker push ferchoslim/catalyst-user-service:latest
