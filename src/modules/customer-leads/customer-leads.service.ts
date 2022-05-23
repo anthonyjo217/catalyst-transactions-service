024909a2c0e71ab6cfecf2e341db51af165b5968
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-05-23 15:33:07 -0500

    netsuite

--- src/modules/customer-leads/customer-leads.service.ts
+++ src/modules/customer-leads/customer-leads.service.ts
@@ -333,11 +333,15 @@ export class CustomerLeadsService {
       };
 
       const { data } = await firstValueFrom(
-        this.httpService.post(process.env.NETSUITE_SERVICE, request, {
-          headers: {
-            'X-API-KEY': process.env.NETSUITE_API_KEY,
+        this.httpService.post(
+          'https://ns.tissini.cloud/api/v1/1539/1',
+          request,
+          {
+            headers: {
+              'X-API-KEY': process.env.NETSUITE_API_KEY,
+            },
           },
-        }),
+        ),
       );
       return { data };
     } catch (error) {
